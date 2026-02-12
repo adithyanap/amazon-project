a1a8075c885f7e24b56b7b1125744cad6689c5b3
adithyanap <adithyan130407@gmail.com>
2026-02-12 12:19:37 +0530

    changes added

--- data/cart.js
+++ data/cart.js
@@ -1,7 +1,37 @@
 export let cart = [
-    
+    {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 1090,
+    keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images/products/intermediate-composite-basketball.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  }
 ]
-
+function storage(){
+  localStorage.setItem("cart",JSON.stringify(cart));
+}
 export function addToCart(){
   document.querySelectorAll('.addToCart')
   .forEach((button) => {
@@ -29,4 +59,14 @@ export function addToCart(){
     });
     
   });
+
+}
+export function remove(productId){
+  let nCart = [];
+  cart.forEach(function(item){
+    if(item.id  != productId){
+      nCart.push(item);
+    }
+  });
+  cart = nCart;
 }
