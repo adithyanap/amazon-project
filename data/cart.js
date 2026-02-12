07fe7c93a8eeb21b44c25d86997fa75864baac9e
adithyanap <adithyan130407@gmail.com>
2026-02-12 15:54:49 +0530

    previous , increase the cart count and add cart element

--- data/cart.js
+++ data/cart.js
@@ -1,58 +1,38 @@
-export let cart = [
-    {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]
-  },
-  {
-    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127
-    },
-    priceCents: 2095,
-    keywords: [
-      "sports",
-      "basketballs"
-    ]
-  }
+export let  cart= [
+
 ]
 
 export function addToCart(){
   document.querySelectorAll('.addToCart')
   .forEach((button) => {
+    let matchingItem = null;
     button.addEventListener('click', () => {
-      let cartNo = 0;
-      let matchingItem;
+      const buttonId = button.dataset.id;
+      
       cart.forEach((item) => {
-        if (item.id === button.dataset.id) {
+        if (item.id === buttonId ) {
           matchingItem = item;
+          
         }
+        
       });
+      
       if (matchingItem) {
         matchingItem.quantity += 1;
       } else {
         cart.push({
           id: button.dataset.id,
           quantity: 1
+        
         });
+        
       }
-
+      console.log(cart);
+      let cartNo = 0;
       cart.forEach((item) => {
         cartNo += item.quantity;
-      })
+      });
+      
       document.querySelector('.cart-quantity').innerHTML = cartNo;
     });
     
